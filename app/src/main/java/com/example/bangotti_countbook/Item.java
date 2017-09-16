18554aad90a3e86fd8b26d8d84466a658205ffff
Bianca Angotti <bangotti@ualberta.ca>
2017-09-16 13:42:53 -0600

    add new item page and initializations

---
+++ app/src/main/java/com/example/bangotti_countbook/Item.java
@@ -0,0 +1,59 @@
+package com.example.bangotti_countbook;
+
+import java.util.Date;
+
+/**
+ * Created by biancaangotti on 2017-09-16.
+ */
+
+public class Item {
+    private String name;
+    private Date date;
+    private int initialCount;
+    private int currentCount;
+    private String comment;
+
+    public Item(String name, int initialCount) {
+        this.name = name;
+        this.date = new Date();
+        this.initialCount = initialCount;
+        this.currentCount = initialCount;
+    }
+
+    public Item(String name, int initialCount, String comment) {
+        this.name = name;
+        this.date = new Date();
+        this.initialCount = initialCount;
+        this.currentCount = initialCount;
+        this.comment = comment;
+    }
+
+    public void incrementCounter() {
+        this.currentCount += 1;
+    }
+
+    public void decrementCounter() {
+        this.currentCount -= 1;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public int getCurrentCount() {
+        return currentCount;
+    }
+
+    public int getInitialCount() {
+        return initialCount;
+    }
+
+}
--- app/src/main/java/com/example/bangotti_countbook/MainActivity.java
+++ app/src/main/java/com/example/bangotti_countbook/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bangotti_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Add Item button */
+    public void addItem(View View) {
+        Intent intent = new Intent(this, AddNewItem.class);
+        startActivity(intent);
     }
 }
